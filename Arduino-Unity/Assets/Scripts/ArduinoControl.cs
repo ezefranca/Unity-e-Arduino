a64ee9eebb67fbe8408b76588a61ce0963b1a7b6
Ezequiel <ezequiel.ifsp@gmail.com>
2020-03-24 20:31:51 -0300

    added files

---
+++ Arduino-Unity/Assets/Scripts/ArduinoControl.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+public class ArduinoControl : MonoBehaviour
+{
+    public string portName;
+    SerialPort arduino;
+    public static int status = 0;
+
+    void Start()
+    {
+        portName = "/dev/cu.usbserial-1410";
+        arduino = new SerialPort(portName, 9600);
+        arduino.Open();
+    }
+
+    void Update()
+    {
+        if (arduino.IsOpen)
+        {
+            if (Input.GetKey("1"))
+            {
+                arduino.Write("1");
+                status = 1;
+                Debug.Log(1);
+            }
+            else if (Input.GetKey("2"))
+            {
+                arduino.Write("2");
+                status = 2;
+                Debug.Log(2);
+            }
+        }
+    }
+}
+
+/*
+ NET 2.0 (not subset) and will connect. I was able to transmit data, but not to receive. Config is into Edit>ProjectSettings>Player>ApiCompatibilityLevel.
+ */
---
+++ Arduino-Unity/Assets/Scripts/Led.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Led : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Color oldcolor;
+
+    void Start()
+    {
+        oldcolor = GetComponent<Renderer>().material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (ArduinoControl.status == 2)
+        {
+            
+            GetComponent<Renderer>().material.color = oldcolor + Color.white / 2;
+        }
+        else
+        {
+            GetComponent<Renderer>().material.color = oldcolor;
+        }
+    }
+}
